fbe6526eca21bbf3873b48f7ed0ee26b4532bdeb
Gian Libunao <gianlibunao@gmail.com>
2019-04-29 02:20:11 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ counter = 0
+ newArray = []
+ while counter<collection.length
+  if yield(collection[counter])==true
+  newArray<< collection[counter]
+  end
+  counter+=1
+ end
+ newArray
 end
